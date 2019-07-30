91eaa1b47beee450d39ef94726bd07c850b8cf6d
Yohann DANA <yodana@e1r7p1.42.fr>
2019-07-30 12:00:21 +0200

    finir affcihage

--- test_affichage.c
+++ test_affichage.c
@@ -7,6 +7,7 @@ int main(int argc, char** argv)
     /* Initialisation simple */
     SDL_Event event;
     SDL_Renderer* renderer;
+    SDL_Renderer* renderer1;
     SDL_Surface* pSurface = NULL;
     const int P_hor = 640;
     const int P_ver = 480;
@@ -25,28 +26,28 @@ int main(int argc, char** argv)
                                                                   P_hor,
                                                                   P_ver,
                                                                   SDL_WINDOW_SHOWN);
-    // SDL_RenderPresent(renderer);
-     /*SDL_Rect rect = {100, 100, 100, 100};
-    
-    SDL_RenderFillRect(renderer, &rect); 
-   // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-   // SDL_RenderClear(renderer);
-    //SDL_RenderPresent(renderer);
-    SDL_RenderPresent(renderer);*/
-        if( pWindow )
-        {
-            while (event.type != SDL_QUIT)
-            {
                 renderer = SDL_CreateRenderer(pWindow, -1, 0);
-                SDL_RenderDrawLine(renderer, P_hor / 2 , 0, P_hor / 2, P_ver);
-            // SDL_WaitEvent(&event);/* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+             SDL_RenderDrawLine(renderer, P_hor / 2 , 0, P_hor / 2, P_ver);
              SDL_Rect rect = {100, 100, 100, 100};
              SDL_RenderFillRect(renderer, &rect);
-             SDL_Rect rect1 = {P_hor / 2 + (P_hor / 5), 100, 100, 100};
+             SDL_Rect rect1 = {P_hor / 2 , 100, 100, 100};
             SDL_RenderFillRect(renderer, &rect1);
-            SDL_FillRect(pSurface, NULL, SDL_MapRGB(pSurface->format, 255, 255, 255));
+           /* SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_FillRect(pSurface, NULL, SDL_MapRGB(pSurface->format, 255, 255, 255));*/
                 SDL_RenderPresent(renderer);
-            }
+                int c = 0;
+        if( pWindow )
+        {
+            int i = 0;
+           while (c == 0)
+           {
+            SDL_WaitEvent(&event);
+             SDL_Rect rect1 = {P_hor / 2 + i++, 100, 100, 100};
+            SDL_RenderFillRect(renderer, &rect1);
+            SDL_RenderPresent(renderer);
+            if (event.type == SDL_QUIT)
+                c++;
+           }
             SDL_DestroyWindow(pWindow);
         }
         else
