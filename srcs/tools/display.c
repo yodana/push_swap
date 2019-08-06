36f929be5c25595cd466719d743802e7e05b0109
Yohann DANA <yodana@e1r2p7.42.fr>
2019-08-06 19:51:46 +0200

    finir

--- srcs/tools/display.c
+++ srcs/tools/display.c
@@ -11,16 +11,15 @@
 /* ************************************************************************** */
 
 #include "../../includes/checker.h"
+
 void   ft_affichage_window(int P_hor, int P_ver,t_all *res)
 {
     int time = 0;
-    //int time1 = 0;
     int i = res->a->size - 1;
     int size = res->size;
     int int_ma = 200;
     int c = 1;
     SDL_Rect rec;
-    float w,h,x,y;
     SDL_SetRenderDrawColor(res->renderer, 0,0, 0,0);
     SDL_RenderClear(res->renderer);
       while (i >= 0)
@@ -30,14 +29,11 @@ void   ft_affichage_window(int P_hor, int P_ver,t_all *res)
         else
           SDL_SetRenderDrawColor(res->renderer, 200,200, 200,200);
         SDL_RenderDrawLine(res->renderer, P_hor / 2 , 0, P_hor / 2, P_ver);
-         w = ((P_hor / 2) / int_ma) * res->a->numbers[i];
-        rec.w = w;
-        h = P_ver / size;
-        rec.h = h;
-        x = ((P_hor / 2) - w) / 2;
-        rec.x = x;
-        y = P_ver - (h * c);
-        rec.y = y;
+        rec.w = ((P_hor / 2) / int_ma) * res->a->numbers[i];
+        rec.h = P_ver / size;
+        rec.x = ((P_hor / 2) - rec.w) / 2;
+        rec.y = P_ver - (rec.h * c);
+
         c++;
         SDL_RenderFillRect(res->renderer, &rec);
         i--;
@@ -76,30 +72,3 @@ void   ft_affichage_window(int P_hor, int P_ver,t_all *res)
             res->delay = res->delay - 0.01;*/
      }
 }
-
-/*void	ft_affichage(t_all *res)
-{
-	int i;
-
-	i = 0;
-	ft_printf(" A\n");
-	while (i < res->size)
-	{
-		if (i < res->a->size)
-			ft_printf("|%d| ", res->a->numbers[i]);
-		else
-			ft_printf("|| ");
-		i++;
-	}
-	i = 0;
-	ft_printf("\n B\n");
-	while (i < res->size)
-	{
-		if (i < res->b->size)
-			ft_printf("|%d| ", res->b->numbers[i]);
-		else
-			ft_printf("|| ");
-		i++;
-	}
-	ft_printf("\n");
-}*/
